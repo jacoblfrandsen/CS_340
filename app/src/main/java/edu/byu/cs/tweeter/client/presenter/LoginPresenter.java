2544d9abc8c7f94651199ffe089154aaaa99a9af
Jacob <jaocb.l.frandsen@gmail.com>
2023-02-18 20:39:42 -0700

    first commit

---
+++ app/src/main/java/edu/byu/cs/tweeter/client/presenter/LoginPresenter.java
@@ -0,0 +1,26 @@
+package edu.byu.cs.tweeter.client.presenter;
+
+import android.graphics.Bitmap;
+
+import edu.byu.cs.tweeter.client.model.service.UserService;
+import edu.byu.cs.tweeter.client.presenter.AbstractPresenters.AuthPresenter;
+
+public class LoginPresenter extends AuthPresenter {
+    public LoginPresenter(AuthView inView) { super(inView); }
+
+    @Override
+    protected String validateInputs(String firstName, String lastName, String username, String password, Bitmap image) {
+        if (username.charAt(0) != '@') { return "Username must begin with @."; }
+        if (username.length() < 2) { return "Alias must contain 1 or more characters after the @."; }
+        if (password.length() == 0) { return "Password cannot be empty."; }
+        return null;
+    }
+
+    @Override
+    protected void callServiceMethod(String firstName, String lastName, String username, String password, Bitmap image) {
+        new UserService().login(username, password, this);
+    }
+
+    @Override
+    protected String getValidateSuccessMessage() { return "Logging in..."; }
+}
